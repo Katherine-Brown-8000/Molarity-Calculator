e8099f8c77f2a6c78a4f27ddab61b6d505f7521b
Katherine Brown <155707632+Katherine-Brown-8000@users.noreply.github.com>
2024-10-17 00:46:57 -0400

    Update Molarity calculator.py

--- Molarity calculator.py
+++ Molarity calculator.py
@@ -9,16 +9,16 @@ if response.status_code == 200:
 else:
     print('Failed to retrieve the data.')
 
-compound = input("Enter name of your compound in abbreviation such as Cl: ").strip()
-mass = float(input("Enter mass of compound (g): "))
+element = input("Enter name of your element in abbreviation such as Cl: ").strip()
+mass = float(input("Enter mass of element (g): "))
 liters_of_solution = float(input('Enter volume in liters in (L), not (mL): '))
 
-molar_mass = atomic_mass.get(compound)
+molar_mass = atomic_mass.get(element)
 
 if molar_mass is None:
-    print(f"Error: Molar mass for {compound} not found")
+    print(f"Error: Molar mass for {element} not found")
 else:
     moles_of_solute = mass / molar_mass
     Molarity = moles_of_solute / liters_of_solution
 
-print(f"The Molarity of {compound} in {liters_of_solution} is {Molarity}")
+print(f"The Molarity of {element} in {liters_of_solution} is {Molarity}")
